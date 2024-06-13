d1e826de8c4ae4afa5386cbd25a9548a516cf9f1
Dhiliban <dhiliban.baskar@gmail.com>
2024-06-13 21:40:16 +0530

    added changes for ip address

--- src/test/java/com/starAgile/SeleniumGrid/FirefoxTest.java
+++ src/test/java/com/starAgile/SeleniumGrid/FirefoxTest.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -15,13 +16,14 @@ public class FirefoxTest {
 		
 		FirefoxOptions options = new FirefoxOptions();
 		
-		URL url = new URL("http://3.110.159.229:4444/wd/hub");
-		RemoteWebDriver driver = new RemoteWebDriver(url, options);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		URL url = new URL("http://13.233.230.49:4444/wd/hub");
+		WebDriver driver = new RemoteWebDriver(url, options);
+		System.out.println("Connection established with firefox");
+	//	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("www.netflix.com");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	//	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		System.out.println("Title of the page " + driver.getTitle());
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	//	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.quit();
 	}
 
---
+++ src/test/java/com/starAgile/SeleniumGrid/ParallelExecution.java
@@ -0,0 +1,42 @@
+package com.starAgile.SeleniumGrid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParallelExecution {
+	
+	WebDriver driver;
+	@Parameters({"bname"})
+	@Test
+	public void browserTest(String bname) throws MalformedURLException, InterruptedException {
+		if(bname.equalsIgnoreCase("chrome")) {
+			ChromeOptions options = new ChromeOptions();
+			driver = new RemoteWebDriver(new URL("http://3.110.106.232:4444"), options);
+			System.out.println("Connection established with chrome");
+		}
+		if(bname.equalsIgnoreCase("firefox")) {
+			FirefoxOptions options = new FirefoxOptions();
+			driver = new RemoteWebDriver(new URL("http://3.110.106.232:4444"), options);
+			System.out.println("Connection established with firefox");
+		}
+		if(bname.equalsIgnoreCase("edge")) {
+			EdgeOptions options = new EdgeOptions();
+			driver = new RemoteWebDriver(new URL("http://3.110.106.232:4444"), options);
+			System.out.println("Connection established with edge");
+		}
+		Thread.sleep(5000);
+		driver.get("https://www.amazon.in");
+		Thread.sleep(3000);
+		System.out.println("Application executing parallelly");
+		driver.quit();
+	}
+	
+}
