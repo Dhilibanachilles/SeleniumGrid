d4b05a11a9307bc94e61d1a0a160ee16f0c92a88
Dhiliban <dhiliban.baskar@gmail.com>
2024-06-19 15:36:50 +0530

    fixed the connection problem with browser

--- src/test/java/grid/EdgeTest.java
+++ src/test/java/grid/EdgeTest.java
@@ -1,4 +1,4 @@
-package com.starAgile.SeleniumGrid;
+package grid;
 
 import java.net.MalformedURLException;
 import java.net.URL;
--- src/test/java/grid/ParallelExecution.java
+++ src/test/java/grid/ParallelExecution.java
@@ -1,4 +1,4 @@
-package com.starAgile.SeleniumGrid;
+package grid;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,23 +18,33 @@ public class ParallelExecution {
 	@Test
 	public void browserTest(String bname) throws MalformedURLException, InterruptedException {
 		if(bname.equalsIgnoreCase("chrome")) {
+			System.out.println("Connection established with chrome ongoing");
 			ChromeOptions options = new ChromeOptions();
-			driver = new RemoteWebDriver(new URL("http://13.201.42.242:4444"), options);
+		//	options.addArguments("--headless");
+		//	options.addArguments("--no-sandbox");
+	    //    options.addArguments("--disable-dev-shm-usage");
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"),options);
 			System.out.println("Connection established with chrome");
 		}
 		if(bname.equalsIgnoreCase("firefox")) {
 			FirefoxOptions options = new FirefoxOptions();
-			driver = new RemoteWebDriver(new URL("http://13.201.42.242:4444"), options);
+		//	options.addArguments("--headless");
+		//	options.addArguments("--no-sandbox");
+	    //  options.addArguments("--disable-dev-shm-usage");
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"),options);
 			System.out.println("Connection established with firefox");
 		}
 		if(bname.equalsIgnoreCase("edge")) {
 			EdgeOptions options = new EdgeOptions();
-			driver = new RemoteWebDriver(new URL("http://13.201.42.242:4444"), options);
+		//	options.addArguments("--headless");
+		//	options.addArguments("--no-sandbox");
+	    //  options.addArguments("--disable-dev-shm-usage");
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"),options);
 			System.out.println("Connection established with edge");
 		}
-		Thread.sleep(5000);
+		Thread.sleep(20000);
 		driver.get("https://www.amazon.in");
-		Thread.sleep(3000);
+		Thread.sleep(20000);
 		System.out.println("Application is being executed");
 		driver.quit();
 	}
