b861633721be1ab060951003fb477be9dd7e6a04
Dhiliban <dhiliban.baskar@gmail.com>
2024-06-19 15:49:00 +0530

    refactored in main class file

--- src/test/java/grid/ParallelExecution.java
+++ src/test/java/grid/ParallelExecution.java
@@ -18,34 +18,24 @@ public class ParallelExecution {
 	@Test
 	public void browserTest(String bname) throws MalformedURLException, InterruptedException {
 		if(bname.equalsIgnoreCase("chrome")) {
-			System.out.println("Connection established with chrome ongoing");
 			ChromeOptions options = new ChromeOptions();
-		//	options.addArguments("--headless");
-		//	options.addArguments("--no-sandbox");
-	    //    options.addArguments("--disable-dev-shm-usage");
 			driver = new RemoteWebDriver(new URL("http://localhost:4444"),options);
 			System.out.println("Connection established with chrome");
 		}
 		if(bname.equalsIgnoreCase("firefox")) {
 			FirefoxOptions options = new FirefoxOptions();
-		//	options.addArguments("--headless");
-		//	options.addArguments("--no-sandbox");
-	    //  options.addArguments("--disable-dev-shm-usage");
 			driver = new RemoteWebDriver(new URL("http://localhost:4444"),options);
 			System.out.println("Connection established with firefox");
 		}
 		if(bname.equalsIgnoreCase("edge")) {
 			EdgeOptions options = new EdgeOptions();
-		//	options.addArguments("--headless");
-		//	options.addArguments("--no-sandbox");
-	    //  options.addArguments("--disable-dev-shm-usage");
 			driver = new RemoteWebDriver(new URL("http://localhost:4444"),options);
 			System.out.println("Connection established with edge");
 		}
 		Thread.sleep(20000);
 		driver.get("https://www.amazon.in");
 		Thread.sleep(20000);
-		System.out.println("Application is being executed");
+		System.out.println("Application is being executed parallelly");
 		driver.quit();
 	}
 	
